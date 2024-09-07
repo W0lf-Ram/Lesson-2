2e4e34e7b5441613c084f450705573918f082a22
W0lf-Ram <Wolfromzes@gmail.com>
2024-09-07 19:10:27 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (hours_spent / total_tasks)
+print(f'Курс: {course_name}, всего задач: {total_tasks}, затрачено часов: {hours_spent}, среднее время выполнения: {time_for_one_task} часа.')
